71ba12256885a1b9e5cf26bf629d81e8439b2ddd
Sahar-De <sr.dehbozorgi@gmail.com>
2025-01-07 09:38:11 +0330

    motion add

--- src/components/whyChoosUs.jsx
+++ src/components/whyChoosUs.jsx
@@ -9,7 +9,7 @@ const WhyUs = () => {
     <section className="pt-28 bg-[url('../../public/assets/cp3.svg')] backdrop-blur-sm">
         <div className="container mx-auto pb-16 px-4 sm:px-6 lg:px-8">
             <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-16 sm:gap-24 md:gap-6">
-            <div className="w-full h-full justify-self-end">
+            <motion.div viewport={{once:true}} initial={{opacity:0 , x:-60}} whileInView={{opacity:1, x:1}} transition={{delay:0.5, duration:0.9, easeInOut}} className="w-full h-full justify-self-end">
                 <h2 className="text-3xl text-right font-extrabold text-gray-100 sm:text-4xl">
                 درباره ما
                 </h2>
@@ -21,14 +21,14 @@ const WhyUs = () => {
                 Nunc pharetra massa lectus, a fermentum arcu volutpat vel.
                 </p>
                 <div className="mt-8">
-                <a href="#" className="text-white float-right hover:text-accent-hover text-right font-medium flex items-center">
-                <span className="mx-2 transform rotate-180 block hover:transform hover:rotate-90">→</span>
-                   مطالب بیشتر
-                </a>
+                    <a href="#" className="text-white float-right hover:text-accent-hover text-right font-medium flex items-center">
+                    <span className="mx-2 transform rotate-180 block hover:transform hover:rotate-90">→</span>
+                    مطالب بیشتر
+                    </a>
                 </div>
-            </div>
+            </motion.div>
                 <div className="mt-12  border-4  md:mt-10 bg-[url('../../public/assets/hseHeader.jpg')] shadow-lg h-72 bg-cover bg-no-repeat relative rounded">
-                    <motion.div viewport={{once:true}} initial={{scale:0}} whileInView={{scale:1}} transition={{delay:0.2, duration:0.3, easeInOut}} className='hidden sm:block h-[200px] w-[240px] bg-accent-hover shadow-xl  xl:left-1/2  rounded relative'>
+                    <motion.div viewport={{once:true}} initial={{opacity:0 , x:60}} whileInView={{opacity:1, x:1}} transition={{delay:0.5, duration:0.9, easeInOut}} className='hidden sm:block h-[200px] w-[240px] bg-accent-hover shadow-xl  xl:left-1/2  rounded relative'>
                         <div className="shadow-lg bg-[url('../../public/assets/640-orange-safety-hat-with-message-safety-first-hard-hat-isolated-on-white-background.jpg')] bg-no-repeat bg-cover absolute size-64 rounded -top-1/3 -right-6">
 
                         </div>
